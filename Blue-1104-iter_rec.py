c7622563b9b5af8821a5bcd4dca475aa5060c046
pattarakornBlue <112365305+pattarakornBlue@users.noreply.github.com>
2022-11-10 08:36:28 +0700

    Add files via upload

---
+++ Blue-1104-iter_rec.py
@@ -0,0 +1,32 @@
+def iterative_power(base, exp):
+   '''
+   base: int or float.
+   exp: int >= 0
+   returns: int or float, base^exp
+   '''
+   ans = base
+   for times in range (1, exp):
+      ans *= base
+   return ans
+      
+
+print (iterative_power (5, 3))
+
+
+def recursive_power(base, exp):
+
+  '''
+ 
+  base: int or float.
+
+  exp: int >= 0
+
+  returns: int or float, base^exp
+
+  '''
+  if exp == 0:
+   return 1
+  else:
+   return (base * recursive_power(base, exp - 1)) 
+
+print recursive_power(10, 10)
